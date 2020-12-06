9d2f42834e44db77faf66dd39d35fbe50834d855
It62070071 <54878356+It62070071@users.noreply.github.com>
2020-12-06 19:14:25 +0700

    เพิ่มราคา

--- main gui/Order.java
+++ main gui/Order.java
@@ -12,37 +12,46 @@ import java.io.Serializable;
  * @author annie
  */
 public class Order implements Serializable{
-    private String name;
-    private int numbers;
-    private String type;
+        private String name;
+        private int numbers;
+        private String type;
+        private int prices;
 
-    public Order(String type,String name, int numbers) {
-        this.type = type;
-        this.name = name;
-        this.numbers = numbers;
-    }
+        public Order(String type,String name, int numbers, int prices) {
+            this.type = type;
+            this.name = name;
+            this.numbers = numbers;
+            this.prices = prices;
+        }
 
-    public String getType() {
-        return type;
-    }
+        public String getType() {
+            return type;
+        }
 
-    public void setType(String type) {
-        this.type = type;
-    }
+        public void setType(String type) {
+            this.type = type;
+        }
 
-    public String getName() {
-        return name;
-    }
+        public String getName() {
+            return name;
+        }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+        public void setName(String name) {
+            this.name = name;
+        }
 
-    public int getNumbers() {
-        return numbers;
-    }
+        public int getNumbers() {
+            return numbers;
+        }
 
-    public void setNumbers(int numbers) {
-        this.numbers = numbers;
+        public void setNumbers(int numbers) {
+            this.numbers = numbers;
+        }
+
+        public void setPrice(int prices){
+            this.prices = prices;
+        }
+        public int getPrices(){
+            return prices;
+        }
     }
-}
